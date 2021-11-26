47c9054d439662d4c06954ad02b42f8601bc3385
CherMill <cmille2016@gmail.com>
2021-11-26 11:59:18 -0700

    first commit

---
+++ scheduler.js
@@ -0,0 +1,5 @@
+// use taskmaster and Taskmaster Pro for reference
+//03-Demo Dynamic for appending data example - used a for loop and pulled text content from API data
+
+var presentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+console.log(presentDate);
