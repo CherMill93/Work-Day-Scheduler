b1be574b004c775d2ed1e5b02d90e9f5b834c203
CherMill <cmille2016@gmail.com>
2021-11-26 12:24:14 -0700

    linked JS to HTML and applied working current  date function to <p> element in HTML

--- scheduler.js
+++ scheduler.js
@@ -1,5 +1,6 @@
 // use taskmaster and Taskmaster Pro for reference
 //03-Demo Dynamic for appending data example - used a for loop and pulled text content from API data
 
-var presentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-console.log(presentDate);
+var currentDayElement = document.getElementById('currentDay');
+var currentDate = moment().format('MMMM Do YYYY');
+currentDayElement.innerHTML = currentDate;
