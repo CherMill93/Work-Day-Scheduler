b8895d2a94f30c306a4cff700bc064d2b409c7aa
CherMill <cmille2016@gmail.com>
2021-11-30 18:29:26 -0700

    localStorage success upon refresh of page

--- scheduler.js
+++ scheduler.js
@@ -11,34 +11,6 @@ var currentDate = moment().format('MMMM Do YYYY');
 currentDayElement.innerHTML = currentDate;
 
 
-var taskSaveObject = {}; //can this remain empty? THis is an object, not an array
-
-// if nothing in localStorage, create a new object to track all task status arrays
-// if (!taskSaveObject) {
-//   taskSaveObject = {
-//     taskArea: [],
-//   };
-//   $.each(taskSaveObject, function(list, arr) {
-//     // then loop over sub-array
-//     arr.forEach(function(task) {
-//       saveTasks(task.text, task.date, list);
-//     });
-//   });
-// }
-
-//Press save button to save tasks
-// var saveButton = document.getElementById('save')
-// var saveTasks = function() {
-//   taskSaveObject = JSON.parse(localStorage.getItem("taskArea")) //am I using "taskArea" correctly? "taskArea" is the location where the task is written in the HTML. redundant
-//   localStorage.setItem("taskArea", JSON.stringify(taskSaveObject))
-//   console.log("save button was successful")
-// };
-
-// saveButton.addEventListener("click", saveTasks); //THis is supposed to run the save function when the save button is pressed. Setup correctly?
-
-//save tasks end
-
-
 
 // task text was clicked - I want this area to be rewritten and integrated into the save button
 $(".save").on("click", function() {
@@ -48,40 +20,8 @@ $(".save").on("click", function() {
   var text = $("#taskSpace-"+timeBlock).val().trim();
   console.log(text, timeBlock);
   localStorage.setItem(timeBlock, text);
-  // replace p element with a new textarea
-  // var textInput = $("<textarea>").addClass("taskArea").attr("id","textSpace").val(text);
-  // $(this).replaceWith(textInput);
-
-  // // auto focus new element
-  // textInput.trigger("focus");
 });
 
-// editable field was un-focused
-$(".taskArea").on("blur", "textarea", function(){
-  // get current value of textarea
-  var text = $(this).val();
-
-  // get status type and position in the list
-  console.log('taskArea element', $(this).closest(".taskArea"));
-  var status = $(this)
-    .closest(".taskArea")
-    .attr("id")
-    .replace("taskArea");
-  var index = $(this)
-    .closest("taskArea-item")
-    .index();
-
-  // update task in array and re-save to localstorage
-  // console.log("task save area", JSON.stringify(taskSaveObject));
-  
-  // taskSaveObject.taskArea[index].text = text;
-  // saveTasks();
-
-  // recreate p element
-  var taskP = $("<p>")
-    .addClass("col w-70 border border-primary taskArea")
-    .text(text);
-
-  // replace textarea with new content
-  $(this).replaceWith(taskP);
-});
+for(var i=8; i<= 20; i++){
+  $("#taskSpace-"+ i).val(localStorage.getItem(i))
+}
